8eba3d51b2697ad2b7ae776abcd5c11f0d7636a5
geofmureithi <mureithinjuguna@gmail.com>
2025-01-11 09:22:44 +0300

    fix: handle missing AckLayer

--- src/ack.rs
+++ src/ack.rs
@@ -1,17 +1,19 @@
 use apalis_core::{layers::Ack, response::Response};
 use lapin::{options::BasicAckOptions, Error};
-use serde::{de::DeserializeOwned, Serialize};
 
 use crate::{utils::AmqpContext, AmqpBackend};
 
-impl<M: Send + Sync + Serialize + DeserializeOwned + 'static, Res: Send + Sync + 'static>
-    Ack<M, Res> for AmqpBackend<M>
-{
+impl<M: Send + 'static, Res: Send + Sync + 'static> Ack<M, Res> for AmqpBackend<M> {
     type AckError = Error;
     type Context = AmqpContext;
-    async fn ack(&mut self, ctx: &AmqpContext, _res: &Response<Res>) -> Result<(), Error> {
-        self.channel
-            .basic_ack(ctx.tag().value(), BasicAckOptions::default())
-            .await
+    async fn ack(&mut self, ctx: &AmqpContext, res: &Response<Res>) -> Result<(), Error> {
+        if res.is_success() {
+            self.channel
+                .basic_ack(ctx.tag().value(), BasicAckOptions::default())
+                .await?;
+        } else {
+            tracing::warn!("Task {} failed and will not be acknowledged.", res.task_id);
+        }
+        Ok(())
     }
 }
